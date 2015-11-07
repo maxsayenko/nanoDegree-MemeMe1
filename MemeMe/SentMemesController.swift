8f9e2213dac4b34a42ea4b93510e9c9d5fe91023
Max Saienko <msaienko@ancestry.com>
2015-11-07 00:13:06 -0800

    connection buttons

--- MemeMe/SentMemesController.swift
+++ MemeMe/SentMemesController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class SentMemesController: UIViewController {
 
+    @IBAction func editTouch(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func addTouch(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func tableTouch(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func gridTouch(sender: UIBarButtonItem) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
