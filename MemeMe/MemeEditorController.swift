e3b026c53b29ad1f10763e13bd99f20d52b26e8a
Max Saienko <msaienko@ancestry.com>
2015-11-26 21:00:58 -0800

    creating a model

--- MemeMe/MemeEditorController.swift
+++ MemeMe/MemeEditorController.swift
@@ -156,15 +156,4 @@ class MemeEditorController: UIViewController, UINavigationControllerDelegate, UI
         
         return memedImage
     }
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
 }
---
+++ MemeMe/MemeModel.swift
@@ -0,0 +1,16 @@
+//
+//  MemeModel.swift
+//  MemeMe
+//
+//  Created by Max Saienko on 11/26/15.
+//  Copyright © 2015 Max Saienko. All rights reserved.
+//
+
+import UIKit
+
+class MemeModel {
+    var topText: String = ""
+    var bottomText: String = ""
+    var originalImage: UIImageView!
+    var memeImage: UIImageView!
+}
