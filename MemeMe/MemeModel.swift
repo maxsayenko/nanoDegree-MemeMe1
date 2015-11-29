48a4327cca4ede69318de75549432a533889e6ca
Max Saienko <msaienko@ancestry.com>
2015-11-28 19:00:36 -0800

    changing class to struct

--- MemeMe/MemeModel.swift
+++ MemeMe/MemeModel.swift
@@ -8,16 +8,9 @@
 
 import UIKit
 
-class MemeModel {
+struct MemeModel {
     var topText: String = ""
     var bottomText: String = ""
     var originalImage: UIImage!
     var memeImage: UIImage!
-    
-    init(topText: String, bottomText: String, originalImage: UIImage, memeImage: UIImage) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memeImage = memeImage
-    }
 }
