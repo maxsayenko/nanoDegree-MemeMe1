1de0d8af1413795ebcd3aedc9854a8b2608e440b
Max Saienko <msaienko@ancestry.com>
2015-12-18 15:17:22 -0800

    clean up debug info

--- MemeMe/Models/CustomPhotoAlbum.swift
+++ MemeMe/Models/CustomPhotoAlbum.swift
@@ -5,18 +5,15 @@
 //  Created by Max Saienko on 12/18/15.
 //  Copyright © 2015 Max Saienko. All rights reserved.
 //
-
 import Photos
 
 class CustomPhotoAlbum {
-    
     static let albumName = "MemeMe"
     static let sharedInstance = CustomPhotoAlbum()
     
     var assetCollection: PHAssetCollection!
     
     init() {
-        print("Init on CustomPhotoAlbum")
         func fetchAssetCollectionForAlbum() -> PHAssetCollection! {
             
             let fetchOptions = PHFetchOptions()
@@ -30,26 +27,18 @@ class CustomPhotoAlbum {
                 print(firstObject)
                 return collection.firstObject as! PHAssetCollection
             }
-            
-            print("-- 1 --")
-            
+
             return nil
         }
         
         if let assetCollection = fetchAssetCollectionForAlbum() {
             self.assetCollection = assetCollection
-            print("-- 2 --")
-            print(self.assetCollection)
             return
         }
-        
-        
-        print("-- 3 --")
         
         PHPhotoLibrary.sharedPhotoLibrary().performChanges({
             PHAssetCollectionChangeRequest.creationRequestForAssetCollectionWithTitle(CustomPhotoAlbum.albumName)
             }) { success, _ in
-                print("-- 4 --")
                 print(success)
                 if success {
                     self.assetCollection = fetchAssetCollectionForAlbum()
@@ -58,9 +47,9 @@ class CustomPhotoAlbum {
     }
     
     func saveImage(image: UIImage) {
-        
         if assetCollection == nil {
-            return   // If there was an error upstream, skip the save.
+            // If there was an error upstream, skip the save.
+            return
         }
         
         PHPhotoLibrary.sharedPhotoLibrary().performChanges(
