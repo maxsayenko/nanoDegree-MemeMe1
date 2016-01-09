6be12f636cbec9c498ff3b60b10cd5ae4067a628
Max Saienko <msaienko@ancestry.com>
2016-01-08 23:23:01 -0800

    connecting data to the table

--- MemeMe/SentMemes/SentMemesTableViewController.swift
+++ MemeMe/SentMemes/SentMemesTableViewController.swift
@@ -10,31 +10,35 @@ import UIKit
 
 class SentMemesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidAppear(animated: Bool) {
         print("table did appear")
-        
+        tableView.reloadData()
     }
     
     override func viewDidLoad() {
         print("table did load")
     }
     
-    override func viewWillDisappear(animated: Bool) {
-        print("table will disappear")
-    }
-    
-    override func viewDidDisappear(animated: Bool) {
-        print("table did disappear")
-    }
-    
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        print("count = \(MemesDataSourceModel.memes.count)")
+        return MemesDataSourceModel.memes.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("tableCell")! as UITableViewCell
-        cell.textLabel?.text = "boo"
+
+        let memeModel = MemesDataSourceModel.memes[indexPath.row] as MemeModel
+        
+        // Set the name and image
+        cell.textLabel?.text = memeModel.topText
+        cell.imageView?.image = memeModel.memeImage
+        
+        // If the cell has a detail label, we will put the evil scheme in.
+//        if let detailTextLabel = cell.detailTextLabel {
+//            detailTextLabel.text = "Scheme: \(villain.evilScheme)"
+//        }
         
         return cell
     }
