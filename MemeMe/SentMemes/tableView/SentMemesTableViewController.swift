3ed7a71e73bbee7443ade52391fbef1524c7c1e9
Max Saienko <msaienko@ancestry.com>
2016-01-15 23:11:17 -0800

    tableView caching

--- MemeMe/SentMemes/tableView/SentMemesTableViewController.swift
+++ MemeMe/SentMemes/tableView/SentMemesTableViewController.swift
@@ -31,17 +31,26 @@ class SentMemesTableViewController: UIViewController, UITableViewDataSource, UIT
         let cell = tableView.dequeueReusableCellWithIdentifier("tableCell") as! TableViewCell
 
         let memeModel = MemesDataSourceModel.memes[indexPath.row] as MemeModel
+        
+        print("\(memeModel.memeImageLocalIdentifier) - \(memeModel.memeImage)")
 
         cell.memeImageView.contentMode = .ScaleAspectFit
-        cell.memeImageView.image = UIImage() // Need to set the image to something, so it can updated later
         
-        if(memeModel.memeImageLocalIdentifier != "") {
+        // Need to set the image to something (even nil), so it can be updated later
+        cell.memeImageView.image = memeModel.memeImage
+        
+        // Data retrieval and caching
+        if(memeModel.memeImage == nil && memeModel.memeImageLocalIdentifier != "") {
             ImageService.getImageFromLocalIdentifier(memeModel.memeImageLocalIdentifier, completionHandler: { (image, error) -> Void in
-                dispatch_async(dispatch_get_main_queue(), {
-                    if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) as! TableViewCell? {
-                        cellToUpdate.memeImageView.image = image
-                    }
-                })
+                if(error == nil) {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) as! TableViewCell? {
+                            cellToUpdate.memeImageView.image = image
+                            memeModel.memeImage = image
+                            print(memeModel.memeImageLocalIdentifier)
+                        }
+                    })
+                }
             })
         }
 
@@ -52,12 +61,8 @@ class SentMemesTableViewController: UIViewController, UITableViewDataSource, UIT
     
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if (editingStyle == .Delete) {
-            //add code ici for when you hit delete
-            print("delete \(indexPath.row)")
             MemesDataSourceModel.DeleteMemeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
         }
     }
-    
-
 }
