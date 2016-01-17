edb64838bef9e480f3fd745c91a4d1b49a4e18e8
Max Saienko <msaienko@ancestry.com>
2016-01-16 16:13:51 -0800

    more clean up

--- MemeMe/SentMemes/tableView/TableViewCell.swift
+++ MemeMe/SentMemes/tableView/TableViewCell.swift
@@ -15,20 +15,6 @@ class TableViewCell: UITableViewCell {
     func populate(model: MemeModel) {
         memeImageView.contentMode = .ScaleAspectFit
         memeImageView.image = model.memeImage
-//        topText.attributedText = getAttributedText(model.topText)
-//        bottomText.attributedText = getAttributedText(model.bottomText)
         descriptionLabel.text = "\(model.topText) ... \(model.bottomText)"
-    }
-    
-    private func getAttributedText(text: String) -> NSAttributedString {
-        let memeTextAttributes = [
-            NSStrokeColorAttributeName : UIColor.blackColor(),
-            NSForegroundColorAttributeName : UIColor.whiteColor(),
-            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 18)!,
-            NSStrokeWidthAttributeName : -3.0
-        ]
-        let attributedString = NSAttributedString(string: text, attributes: memeTextAttributes)
-        
-        return attributedString
     }
 }
