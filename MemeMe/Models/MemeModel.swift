2a90d3a779f9fabd4e5d1864fbb5b21f6ab897ee
Max Saienko <msaienko@ancestry.com>
2016-01-19 19:11:37 -0800

    clean up comments

--- MemeMe/Models/MemeModel.swift
+++ MemeMe/Models/MemeModel.swift
@@ -16,21 +16,4 @@ struct MemeModel {
     var originalImageLocalIdentifier: String = ""
     var memeImage: UIImage?
     var memeImageLocalIdentifier: String = ""
-    
-//    func encodeWithCoder(aCoder: NSCoder!) {
-//        aCoder.encodeObject(topText, forKey: "topText")
-//        aCoder.encodeObject(bottomText, forKey: "bottomText")
-//        aCoder.encodeObject(originalImageLocalIdentifier, forKey: "originalImageLocalIdentifier")
-//        aCoder.encodeObject(memeImageLocalIdentifier, forKey: "memeImageLocalIdentifier")
-//    }
-//    
-//    init(coder aDecoder: NSCoder!) {
-//        topText = aDecoder.decodeObjectForKey("topText") as! String
-//        bottomText = aDecoder.decodeObjectForKey("bottomText") as! String
-//        originalImageLocalIdentifier = aDecoder.decodeObjectForKey("originalImageLocalIdentifier") as! String
-//        memeImageLocalIdentifier = aDecoder.decodeObjectForKey("memeImageLocalIdentifier") as! String
-//    }
-//    
-//    override init() {
-//    }
 }
